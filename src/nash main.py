688d206083f183b16bfdde438309070f6146053d
jarne <jarne.dewaele@ugent.be>
2021-04-16 14:54:13 +0200

    Added flow nashpy + technology & switching costs

--- src/nash main.py
+++ src/nash main.py
@@ -4,12 +4,46 @@ import os
 from definitions import ASSETS_PATH
 from src.payoff import *
 
-payoff_c2c_cc, payoff_5gaa = build_payoff_matrix()
 
-print(payoff_c2c_cc)
-print(payoff_5gaa)
+def get_pure_nash_equilibria():
+    # Step 1: build payoff matrices for both players (the 2 consortia)
+    payoff_c2c_cc, payoff_5gaa = build_payoff_matrix()
+    print(payoff_c2c_cc)
+    print(payoff_5gaa)
 
-phase0_game = nash.Game(payoff_c2c_cc, payoff_5gaa)
-print(phase0_game)
-for eq in phase0_game.vertex_enumeration():
-    print(np.round(eq[0], 2), np.round(eq[1], 2))
+    # Step 2: build the game using the nashpy library
+    phase0_game = nash.Game(payoff_c2c_cc, payoff_5gaa)
+
+    # Step 3: solve the game
+
+    pure_strategies_c2c_cc= {}  # Used for storing all pure strategies
+    pure_strategies_5gaa = {}
+    pure_strategies = {}
+    amt_pure_strat = 0  # Used as keys in the dictionary
+
+    # print(phase0_game)
+
+    for eq in phase0_game.support_enumeration():  # iterate over all equilibria
+        # print(np.round(eq[0], 2), np.round(eq[1], 2))  # separate print of each equilibrium
+
+        # Filter out the mixed strategies
+        pure_strat = True
+        for i in range(2):
+            if np.count_nonzero(eq[i] == 1) != 1:
+                pure_strat = False
+
+        # Only store pure strategies in the dictionary
+        if pure_strat:
+            amt_pure_strat += 1
+            pure_strategies[str(amt_pure_strat)] = [list(np.round(eq[0], 2)), list(np.round(eq[1], 2))]
+            pure_strategies_c2c_cc["C2C-CC strategy " + str(amt_pure_strat)] = list(np.round(eq[0], 2))
+            pure_strategies_5gaa["5GAA strategy " + str(amt_pure_strat)] = list(np.round(eq[1], 2))
+
+    print(pure_strategies)
+    print(pure_strategies_c2c_cc)
+    print(pure_strategies_5gaa)
+
+    # Step 4: update penetration
+
+get_pure_nash_equilibria()
+
--- src/nash test flow.py
+++ src/nash test flow.py
@@ -7,12 +7,13 @@ strategies=[0, 0.25, 0.50, 0.75, 1]
 
 
 def generate_random_payoffmatrix(amount_of_strategies, share=1 / 7):
-    return np.array([[np.round(np.random.random() * share, 2) for i in range(amount_of_strategies)]
+    return np.array([[np.round(np.random.randint(0, 100) * share, 0) for i in range(amount_of_strategies)]
                      for j in range(amount_of_strategies)])
 
 
 player1_payoff_matrix = generate_random_payoffmatrix(amount_of_strategies)
 player2_payoff_matrix = generate_random_payoffmatrix(amount_of_strategies)
+# print(player1_payoff_matrix)
 
 game1 = nash.Game(player1_payoff_matrix, player2_payoff_matrix)
 pure_strategies = {}
@@ -23,7 +24,7 @@ for eq in game1.support_enumeration():
         if np.count_nonzero(eq[i] == 1) != 1:
             pure_strat = False
 
-    if pure_strat == True:
+    if pure_strat:
         amt_pure_strat += 1
         pure_strategies[str(amt_pure_strat)] = [list(np.round(eq[0], 2)), list(np.round(eq[1], 2))]
         print(np.round(eq[0], 2), np.round(eq[1], 2))
