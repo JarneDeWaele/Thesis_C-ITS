df9d2ef6a1bd6ba9444fd9a61b3e07930281d1f2
Jarne De Waele <jarne.dewaele@ugent.be>
2021-04-19 12:24:50 +0000

    Update payoff flow game

--- src/payoff.py
+++ src/payoff.py
@@ -16,14 +16,15 @@ def get_technology_cost_cv2x(cv2x_used):
         return 0
 
 
-def get_network_effects(x, k=10):
+def get_network_effects(x, previous_share, k=10):
     if x < 0 or x > 1:
         raise ValueError("x is not a valid number")
+
     else:
-        return np.around(1 / (1 + np.exp(-k * (x - 0.5))), 3) * 1000
+        return np.around(1 / (1 + np.exp(-k * (x + previous_share - 0.5))), 3) * 1000
 
 
-def get_switching_cost(switching):
+def get_switching_cost(switching, switching_share):
     """
     The initial software development costs would be approximately €1mn per model,
     based on a team of ten engineers working for a year to develop the software (BMW 2014)
@@ -36,53 +37,71 @@ def get_switching_cost(switching):
     :return: switching cost for implementing other tech, in million euros
     """
     if switching:
-        return -200
+        return -200 * switching_share
     else:
         return 0
     # return 0.5*x
 
 
-def get_total_payoff(itsg5_share, cv2x_share, switching, cv2x_used):
+def get_total_payoff(itsg5_share, cv2x_share, switching, switching_share, cv2x_used, previous_share_itsg5, previous_share_cv2x):
     """
 
+    :param switching_share:
     :param itsg5_share:
     :param cv2x_share:
     :param switching:
     :param cv2x_used:
     :return:
     """
-    total_payoff = get_switching_cost(switching) + \
+    # To think: relative to share of tech or not ?
+    # is itsg5_share*1/7 correct ?
+    total_payoff = get_switching_cost(switching, switching_share) + \
                    np.around(get_technology_cost_cv2x(cv2x_used) * cv2x_share, 0) \
-                   + np.around(get_network_effects(itsg5_share*1/7), 0) + \
-                   np.around(get_network_effects(cv2x_share*1/7), 0) + 1000
+                   + np.around(get_network_effects(itsg5_share*1/7, previous_share_itsg5), 0) + \
+                   np.around(get_network_effects(cv2x_share*1/7, previous_share_cv2x), 0)
     return total_payoff
 
-
-def build_payoff_matrix(strategies=[0, 0.25, 0.50, 0.75, 1]):  # assumption: both consortia are equally important
-    # penetration = get_penetration()
-    shares = [25.7, 22.7, 10.2, 7.2, 7.1, 6.9, 6.2, 5.1]
+def get_consortia_shares():
+    shares = [25.7, 22.7, 10.2, 7.2, 7.1, 6.9, 6.2, 5.1]    # add source
     total_share = np.sum(shares)
     share_c2c_cc = (25.7/2+10.2/2+7.1/2+6.9)/total_share
     share_5gaa = (25.7/2+22.7+10.2/2+7.2+7.1/2+6.2+5.1)/total_share
+    return share_c2c_cc, share_5gaa
+
+def build_payoff_matrix(strategies):
+    previous_share_itsg5 = get_penetration()['itsg5']
+    previous_share_cv2x = get_penetration()['cv2x']
+    print(previous_share_itsg5, previous_share_cv2x)
+    shares = [25.7, 22.7, 10.2, 7.2, 7.1, 6.9, 6.2, 5.1]    # add source
+    total_share = np.sum(shares)
+    share_c2c_cc, share_5gaa = get_consortia_shares()
 
     payoff_matrix_c2c_cc = np.zeros((len(strategies), len(strategies)))
     payoff_matrix_5gaa = np.zeros((len(strategies), len(strategies)))
+
+    # Initialize payoff matrixes
     for count_c2c_cc, strategy_c2c_cc in enumerate(strategies):  # The base strategy here is ITS_G5
-        for count_5gaa, strategy_5gaa in enumerate(strategies):
+        for count_5gaa, strategy_5gaa in enumerate(strategies): # change: not nested
             itsg5_share = strategy_c2c_cc * share_c2c_cc + strategy_5gaa * share_5gaa
             cv2x_share = 1 - itsg5_share
+            switching = False
+
             if cv2x_share > 0:
                 cv2x_used = True
             else:
                 cv2x_used = False
-            if itsg5_share * cv2x_share != 0:
+
+            if strategy_c2c_cc <= 1:
                 switching = True
-            else:
-                switching = False
             payoff_matrix_c2c_cc[count_c2c_cc, count_5gaa] = \
-                get_total_payoff(itsg5_share, cv2x_share, switching, cv2x_used)
+                np.around(get_total_payoff(itsg5_share, cv2x_share, switching, cv2x_share, cv2x_used, previous_share_itsg5, previous_share_cv2x), 1)
+            switching = False
+
+            if strategy_5gaa > 0:
+                switching = True
             payoff_matrix_5gaa[count_c2c_cc, count_5gaa] = \
-                get_total_payoff(itsg5_share, cv2x_share, switching, cv2x_used)
+                np.around(get_total_payoff(itsg5_share, cv2x_share, switching, itsg5_share, cv2x_used, previous_share_itsg5, previous_share_cv2x), 1)
+
             array_c2c_cc = np.array(payoff_matrix_c2c_cc)
             array_5gaa = np.array(payoff_matrix_5gaa)
     return array_c2c_cc, array_5gaa
